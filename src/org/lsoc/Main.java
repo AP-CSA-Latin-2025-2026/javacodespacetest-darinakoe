0c45fbecc87b3118350106f3f5833ffb26cde5c6
darinakoe <156979980+darinakoe@users.noreply.github.com>
2025-08-27 16:34:18 +0000

    Completed the homework on time

--- src/org/lsoc/Main.java
+++ src/org/lsoc/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println("Hello, my name is Darina. A little bit about me is that I am born in the UK and can speak Bulgarian. This summer I went to the UK, Amsterdam and Puerto Rico. it was so fun asnd I got to spend time with my famlily. One of my hobbies is mountain climbing and I actually summited Mont Blanc last summer!");
     }
 }
